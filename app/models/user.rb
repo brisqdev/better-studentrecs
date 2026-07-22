75c5941e6a7539acc738f4d58584ee729dd84888
Kabir Sekhon <tjaynj@gmail.com>
2026-07-22 08:06:58 -0400

    validates public token

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   has_secure_token :public_token, on: :initialize
   validates :role, inclusion: { in: %w[system_admin admissions_officer school_counselor teacher applicant] }
+  validates :public_token, uniqueness: true, allow_nil: true
 end
