b378c81ab43c7f235743821531b1628cefc36974
Kabir Sekhon <tjaynj@gmail.com>
2026-07-21 20:17:30 -0400

    login
    used rails generate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
+  include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Allow non-logged in users to access the homepage
+  allow_unauthenticated_access only: :index
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
