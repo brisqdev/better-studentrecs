7df58e4bae66c28552f86d30deeb5bfe992fa6a5
Kabir Sekhon <tjaynj@gmail.com>
2026-07-21 20:31:24 -0400

    real base auth flow
    fixed all auth to work basic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,24 @@
 class SessionsController < ApplicationController
-  allow_unauthenticated_access only: %i[ new create ]
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
+  # Allows guests to view the root login page and submit credentials
+  allow_unauthenticated_access only: [ :new, :create ]
+  
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to root_url, alert: "Try again later." }
 
   def new
+    # Renders login view on the / index path
   end
 
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
-      start_new_session_for user
-      redirect_to after_authentication_url
+      start_new_session_for(user)
+      redirect_to after_authentication_url, notice: "Logged in successfully!"
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      redirect_to root_url, alert: "Invalid credentials. Please try again."
     end
   end
 
   def destroy
     terminate_session
-    redirect_to new_session_path, status: :see_other
+    redirect_to root_url, notice: "Logged out successfully!"
   end
 end
