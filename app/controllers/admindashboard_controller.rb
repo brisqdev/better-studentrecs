c453c366ee083e0a533cc29217f04a6b2c246975
Kabir Sekhon <tjaynj@gmail.com>
2026-07-23 21:16:22 -0400

    created delete route
    Created a delete route for system admin to delete admissions officers
    
    Additionally: drafted a create route for the system admin to create admissions officers

--- app/controllers/admindashboard_controller.rb
+++ app/controllers/admindashboard_controller.rb
@@ -1,4 +1,6 @@
 class AdmindashboardController < ApplicationController
+  before_action :ensure_admin!, only: [ :delete_staff, :create_staff ]
+
   def index
     user = Current.user
 
@@ -7,6 +9,34 @@ class AdmindashboardController < ApplicationController
       nil
     elsif user.role != "system_admin"
       redirect_to dashboard_url
+    end
+
+    @admissionsStaff = User.where(role: "admissions_officer")
+  end
+
+
+  # Staff Management Functions
+
+  def delete_staff
+    User.find(params[:id]).destroy
+
+    # Redirect back to admin dashboard path
+    redirect_to admindashboard_path, notice: "User successfully removed."
+  end
+
+  def create_staff
+    User.create(email_address: params[:email_address], password: params[:password], role: "admissions_officer")
+
+    # Redirect back to admin dashboard path
+    redirect_to admindashboard_path, notice: "User successfully created."
+  end
+
+  private
+
+  def ensure_admin!
+    unless user&.role == "system_admin"
+      # Redirect to root path as non-authorized user
+      redirect_to root_path, alert: "Not authorized."
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get "applicantdashboard/index"
-  get "teacherdashboard/index"
-  get "counselordashboard/index"
-  get "staffdashboard/index"
-  get "admindashboard/index"
+  # get "applicantdashboard/index"
+  # get "teacherdashboard/index"
+  # get "counselordashboard/index"
+  # get "staffdashboard/index"
+  # get "admindashboard/index"
+
   # FAQs page
   get "/faqs", to: "faqs#index", as: :faqs
 
@@ -17,10 +18,17 @@ Rails.application.routes.draw do
 
   # Distinct Dashboard Routes
   get "/dashboard", to: "dashboard#index", as: :dashboard
+
   get "/admindashboard", to: "admindashboard#index", as: :admindashboard
+  delete "/admindashboard/users/:id", to: "admindashboard#delete_staff", as: :delete_staff
+  post "/admindashboard/users/:email_address/:password", to: "admindashboard#create_staff", as: :create_staff
+
   get "/staffdashboard", to: "staffdashboard#index", as: :staffdashboard
+
   get "/counselordashboard", to: "counselordashboard#index", as: :counselordashboard
+
   get "/teacherdashboard", to: "teacherdashboard#index", as: :teacherdashboard
+
   get "/applicantdashboard", to: "applicantdashboard#index", as: :applicantdashboard
 
   # Standard authentication routes (Excluding /session/new)
